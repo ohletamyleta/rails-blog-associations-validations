13e035451ef9c708a5772e55fd932c1c26f5e984
ohletamyleta <ohletamyleta@gmail.com>
2021-02-24 14:02:33 -0500

    Done.

---
+++ db/migrate/02_create_posts_tags.rb
@@ -0,0 +1,11 @@
+class CreatePostsTags < ActiveRecord::Migration[5.0]
+  def change
+    create_table :posts_tags do |t|
+      t.integer :post_id
+
+      t.integer :tag_id
+
+      t.timestamps null: false
+    end
+  end
+end
